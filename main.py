d26e6d887efb68a90b9b828ed6955cd3d90edf5e
bxrdxs <88906083+bxrdxs@users.noreply.github.com>
2023-02-18 08:12:11 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+# Set page title
+st.set_page_config(page_title="My Streamlit App")
+
+# Set page header
+st.header("My Streamlit App")
+
+# Get value from user
+user_input = st.number_input("Enter a value", min_value=0.0, max_value=100.0, value=50.0, step=1.0)
+
+# Display output
+st.write("The value you entered is:", user_input)
